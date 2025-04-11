5ce45c6efc3321e4beb3ffd0d5e0332af9dd5b42
Documenter.jl <documenter@juliadocs.github.io>
2025-04-11 05:59:19 +0000

    build based on 7523d54

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LabControl","category":"page"},{"location":"#LabControl","page":"Home","title":"LabControl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LabControl.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LabControl]","category":"page"}]
+}
